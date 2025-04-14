5f0377bef22026d6039c5de1a4e30fad3d47b892
ProMe <jeungpjw104@gmail.com>
2025-04-14 08:31:16 -0400

    Update Game.js

--- Game.js
+++ Game.js
@@ -5,13 +5,14 @@ function move(x,y){
   circle.style.top=`${y-12}px`;
 }
 
+document.addEventListener('touchmove', (e)=>{
+  if (e.touches.length > 1) return; // Allow multitouch gestures
+  e.preventDefault();
+}, { passive: false });
+
 document.addEventListener("touchmove", (e)=>{
   const touches=e.touches[0];
   move(touches.clientX,touches.clientY);
 })
 
-document.addEventListener('touchmove', (e)=>{
-  if (e.touches.length > 1) return; // Allow multitouch gestures
-  e.preventDefault();
-}, { passive: false });
 
