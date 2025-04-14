85cf54de958c9f884c3730b326f5c3e55f3937fe
ProMe <jeungpjw104@gmail.com>
2025-04-14 13:31:30 -0400

    Update Game.js
    just testing

--- Game.js
+++ Game.js
@@ -10,9 +10,9 @@ document.addEventListener("touchmove", (e)=>{
   move(touches.clientX,touches.clientY);
 })
 
-document.addEventListener("mousemove", (e)=>{
+circle.addEventListener("click",document.addEventListener("mousemove", (e)=>{
   move(e.clientX,e.clientY);
-})
+}))
 
 
 function getRandomColor(){
