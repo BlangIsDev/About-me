f700e9b5dde5a3677784a8a3fa382e2b80f08649
ProMe <jeungpjw104@gmail.com>
2025-04-14 08:29:06 -0400

    Update Game.js

--- Game.js
+++ Game.js
@@ -9,3 +9,9 @@ document.addEventListener("touchmove", (e)=>{
   const touches=e.touches[0];
   move(touches.clientX,touches.clientY);
 })
+
+document.addEventListener('touchmove', (e)=>{
+  if (e.touches.length > 1) return; // Allow multitouch gestures
+  e.preventDefault();
+}, { passive: false });
+
